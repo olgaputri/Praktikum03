b8f1128f8cf6eb711e324ef9a9d5391abee5bc66
Olga riswanti putri <32381022+olgaputri@users.noreply.github.com>
2017-11-06 11:23:52 +0700

    tinggi badan

--- tinggibadan.cpp
+++ tinggibadan.cpp
@@ -4,4 +4,21 @@ using namespace std;
 
 int main() 
 {
+    float bb;
+    int tb;
+    
+    cout << "Silahkan masukkan tinggi badan(cm) : "; cin >>tb;
+    cout << "Silahkan masukkan berat badan(kg) : " ;
+    cin >> bb;
+    
+    if (bb<tb/2.5)
+       cout << "Underweight" << endl;
+    else if (tb/2.5 <= bb <= tb/2.3)
+       cout << "Normal" << endl;
+    else if (tb/2.3 < bb)
+                 cout << "" << endl;
+
+                 
+                 
+                 return 0;
     }
