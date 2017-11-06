8f3edb915167a527d18601d8f651adb3565d31bb
Olga riswanti putri <32381022+olgaputri@users.noreply.github.com>
2017-11-06 11:21:18 +0700

    jenisbilangan

--- jenisbilangan.cpp
+++ jenisbilangan.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main(){
+	int x;
+	cout<<""<<endl;
+	cout<<"Input Bilangan : ";cin>>x;
+	cout<<""<<endl;
+	if (x < 0)
+	{
+		cout<<"yang anda input adalah bilangan negatif"<<endl;
+	}
+	
+	else if (x>0)
+	{
+		cout<<"yang anda input adalah bilangan positif"<<endl;
+	}
+	else if (x==0)
+	
+		cout<<"yang anda input adalah bilangan nol "<<endl;
 
+	return 0;
 }
